4e5c42b2c1977ad9613c1598ad2bc75aaa40e27e
LENOVO PC <LENOVO PC@LENOVO>
2021-07-27 10:45:06 +0530

    WH-2460316-first time update

---
+++ src/main/java/com/jpa/pagination/SpringbootgithelloApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.pagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootgithelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootgithelloApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jpa/pagination/git/Messagepasser.java
@@ -0,0 +1,15 @@
+package com.jpa.pagination.git;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class Messagepasser implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Spring boot git");
+
+	}
+
+}
