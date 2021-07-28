c8828b8ee40888a0fc298bd31f35b53c58e09b9f
LENOVO PC <LENOVO PC@LENOVO>
2021-07-28 11:19:13 +0530

    WH-123-second time updated

--- src/main/java/com/jpa/pagination/git/Messagepasser.java
+++ src/main/java/com/jpa/pagination/git/Messagepasser.java
@@ -9,6 +9,7 @@ public class Messagepasser implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Spring boot git");
+		System.out.println("Hello GIT");
 
 	}
 
